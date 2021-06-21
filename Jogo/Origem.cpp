97b742fd9610e7b7e59c7844ae51f4f06ca683c8
Diogo <diogodasilvagouveia@gmail.com>
2021-06-21 14:03:08 -0300

    linkando SFML

--- Jogo/Origem.cpp
+++ Jogo/Origem.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include "stdafx.h"
 
 int main()
 {
-	std::cout << "test";
-	return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
